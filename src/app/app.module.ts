f7dbc3720181811b66b0fb3b8fd63624456b0413
Koen De Deckers <koend@FUJITSU-KDD.ebema.local>
2020-07-14 21:50:33 +0200

    start van top navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbartopComponent } from './navbartop/navbartop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbartopComponent
   ],
   imports: [
     BrowserModule,
